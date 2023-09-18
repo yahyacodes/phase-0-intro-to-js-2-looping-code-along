bb018589bc2c1e92372714b2f8e573bde97d7471
yahyacodes <yahya.codin@gmail.com>
2023-09-18 16:43:02 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names) {
+  const messages = [];
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+  }
+  return messages;
+}
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
